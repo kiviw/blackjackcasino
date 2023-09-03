e5b93918f6a87112f886316d3f7fbac736e1a2f2
kiviw <100179542+kiviw@users.noreply.github.com>
2023-09-03 09:18:16 +0300

    Create monero_config.php

---
+++ monero_integration/monero_config.php
@@ -0,0 +1,9 @@
+<?php
+// monero_config.php
+
+// Your main Monero wallet address
+$mainWalletAddress = 'your-main-wallet-address';
+
+// Your Monero secret view key
+$secretViewKey = 'your-secret-view-key';
+?>
